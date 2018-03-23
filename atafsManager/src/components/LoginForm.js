cf398d6e9cb3bfb49fd8e0a82015b5239085c852
Americo Tomas <americo.tomas@fuseuniversal.com>
2018-03-23 20:25:44 +0000

    120. Redux Thunk in Practice Continued

--- atafsManager/src/components/LoginForm.js
+++ atafsManager/src/components/LoginForm.js
@@ -1,6 +1,6 @@
 import React, {Component} from 'react';
 import { Card, CardSection, Input, Button } from './common'
-import { emailChanged } from "../actions";
+import { emailChanged, passwordChanged, loginUser } from "../actions";
 
 import { connect } from 'react-redux';
 
@@ -10,13 +10,24 @@ class LoginForm extends Component {
         this.props.emailChanged(text)
     }
 
+    onPasswordChange(text) {
+        this.props.passwordChanged(text)
+    }
+
+    onButtonPress() {
+        const { email, password } = this.props;
+
+        console.log('onButtonPress email && password', this.props.email, this.props.password)
+        this.props.loginUser({ email, password })
+    }
+
     render() {
         return (
             <Card>
                 <CardSection>
                     <Input
                         label="Email"
-                        placeHolder="email@email.com"
+                        placeholder="email@email.com"
                         onChangeText={this.onEmailChange.bind(this)}
                         value={this.props.email}
                     />
@@ -25,11 +36,13 @@ class LoginForm extends Component {
                     <Input
                         secureTextEntry
                         label="Password"
-                        placeHolder="password"
+                        placeholder="password"
+                        onChangeText={this.onPasswordChange.bind(this)}
+                        value={this.props.password}
                     />
                 </CardSection>
                 <CardSection>
-                    <Button>
+                    <Button onPress={this.onButtonPress.bind(this)}>
                         Login
                     </Button>
                 </CardSection>
@@ -40,8 +53,13 @@ class LoginForm extends Component {
 
 const mapStateToProps = state => {
     console.log('state', state)
+    const { email, password } = state.auth
 
-    return { email: state.auth.email }
+    return { email, password }
 };
 
-export default connect(mapStateToProps, { emailChanged })(LoginForm)
+export default connect(mapStateToProps, {
+    emailChanged,
+    passwordChanged,
+    loginUser
+})(LoginForm)
--- atafsManager/src/reducers/AuthReducer.js
+++ atafsManager/src/reducers/AuthReducer.js
@@ -1,15 +1,24 @@
-import { EMAIL_CHANGED } from "../actions/types";
+import {
+    EMAIL_CHANGED,
+    PASSWORD_CHANGED
+} from "../actions/types";
 
-const INITIAL_STATE = { email: '' }
+const INITIAL_STATE = {
+    email: '',
+    password: ''
+}
 
 export default (state = INITIAL_STATE, action) => {
-    console.log('action', action)
+    console.log('state', state)
+    console.log('AuthReducer action', action)
 
     switch (action.type) {
         case EMAIL_CHANGED:
             console.log('EMAIL_CHANGED', EMAIL_CHANGED)
-
             return { ...state, email: action.payload }
+        case PASSWORD_CHANGED:
+            console.log('PASSWORD_CHANGED', PASSWORD_CHANGED)
+            return { ...state, password: action.payload }
         default:
             return state
     }
